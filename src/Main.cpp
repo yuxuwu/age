94526856550dea8e8b93d61a3235c82cf93b8e8e
Yu Xuan Wu <yuxu.wu1@gmail.com>
2025-09-23 18:05:48 -0400

    GLM Works

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <glm/glm.hpp>
 
 using namespace std;
 
 int main (int argc, char** argv) {
     cout << "Hello world" << endl;
+
+    /// Test glm
+    glm::vec2 myVec(1.0, 1.0);
+    glm::vec2 myOtherVec(2.0, 2.0);
+    myVec += myOtherVec;
+    cout << "myVec: " << myVec.x << ", " << myVec.y << endl;
+    
+    /// Test Lua
+    /// Test entt
+    /// Test SDL3
+    /// Test OpenGL
 }
